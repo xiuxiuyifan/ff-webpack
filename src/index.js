8f6ca8519845d3a3d639e18333a5f0d040293314
xiuxiuyifan <xiuxiuyifan@163.com>
2021-05-16 17:48:58 +0800

    添加babel-loader和.browserslistrc

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var fn = () =>{
+  console.log(893257489237)
+  const a = 100
+  const b = 200
+  const c = a + b
+  console.log(c)
+}
+
+fn()
+console.log('fdhaskdfhakjhk')
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
